6f6fecfc2806324824add3e00fbcbcdd2bb5c48c
skinnerv <piotr.ghost@gmail.com>
2023-03-18 17:41:25 +0100

    Create ping-sweep.sh
    Ping sweep bash script - single thread

---
+++ ping_sweep/ping-sweep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#This is a bash script ping sweep
+#Inserting the first 3 octets of the IP address, the starting value of the last octets and the ending value of the last octets.
+#
+for i in $(seq $2 $3);do
+        ip="$1.$i"
+        #echo $ip
+        if ping -c 1 $ip >/dev/null; then
+                echo $ip
+        fi
+done
