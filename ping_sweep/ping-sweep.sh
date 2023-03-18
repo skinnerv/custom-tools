410bfd1e39eb088e078b46c1a25f3242a11e11d9
skinnerv <piotr.ghost@gmail.com>
2023-03-18 17:58:34 +0100

    Update ping-sweep.sh

--- ping_sweep/ping-sweep.sh
+++ ping_sweep/ping-sweep.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 #
 #This is a bash script ping sweep
 #Inserting the first 3 octets of the IP address, the starting value of the last octets and the ending value of the last octets.
